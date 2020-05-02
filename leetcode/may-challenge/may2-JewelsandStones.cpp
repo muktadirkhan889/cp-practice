1126ff33a72ee70a8f04a9b198014b2cfe31a898
Muktadir Khan <muktadirkhan889@gmail.com>
2020-05-02 15:54:02 +0530

    Create may2-JewelsandStones.cpp

---
+++ leetcode/may-challenge/may2-JewelsandStones.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int count = 0;
+        for(int i=0;i<S.size();i++) {
+            for(int j=0;j<J.size();j++) {
+                if(J[j]==S[i]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
