c9af0abca9d6b8d75041a5c75b156ecc03d7d1da
Muktadir Khan <muktadirkhan889@gmail.com>
2020-05-10 02:50:28 +0530

    Add link to the problem

--- leetcode/may-challenge/week2/may8-CheckIfItIsaStraightLine.cpp
+++ leetcode/may-challenge/week2/may8-CheckIfItIsaStraightLine.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3323/
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
