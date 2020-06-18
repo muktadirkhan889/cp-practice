d4da1d0732ef04bb9ee9761f753966d3eeb18501
Muktadir Khan <muktadirkhan889@gmail.com>
2020-06-18 17:46:09 +0530

    Create Binary-Search.cpp

---
+++ leetcode/Binary-Search.cpp
@@ -0,0 +1,18 @@
+/* https://leetcode.com/explore/learn/card/binary-search/138/background/1038/ */
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while(low<=high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid]==target) {
+                return mid;
+            } else if(nums[mid]<target) {
+                low = mid + 1;
+            } else {
+                high  = mid -1;
+            }
+        }
+        return -1;
+    }
+};
