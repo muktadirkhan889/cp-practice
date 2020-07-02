9ebd0cd538301aef1eb802b938285d08fb0adce6
Muktadir Khan <muktadirkhan889@gmail.com>
2020-07-03 01:09:44 +0530

    Create tree-traversals-inorder-preorder-and-postorder.cpp

---
+++ GeeksforGeeks/Tree/#include <bits/tree-traversals-inorder-preorder-and-postorder.cpp
@@ -0,0 +1,59 @@
+/* https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/ */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+void printPostorder(struct Node *node) {
+    if (node == NULL) {
+        return;
+    }
+    printPostorder(node->left);
+    printPostorder(node->right);
+    cout << node->data << " ";
+}
+
+void printInorder(struct Node *node) {
+    if (node == NULL) {
+        return;
+    }
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+
+void printPreorder(struct Node *node) {
+    if (node == NULL) {
+        return;
+    }
+    cout << node->data << " ";
+    printPreorder(node->left);
+    printPreorder(node->right);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Preorder: ";
+    printPreorder(root);
+    cout << "\nInorder: ";
+    printInorder(root);
+    cout << "\nPostorder: ";
+    printPostorder(root);
+
+    return 0;
+}
