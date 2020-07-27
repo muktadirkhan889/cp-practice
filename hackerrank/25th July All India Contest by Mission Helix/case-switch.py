be4f7ba477af8f8cef0ce8ce12a96f6b7b9d4d73
Muktadir Khan <muktadirkhan889@gmail.com>
2020-07-28 01:39:03 +0530

    Create case-switch.py

---
+++ hackerrank/25th July All India Contest by Mission Helix/case-switch.py
@@ -0,0 +1,5 @@
+/* https://www.hackerrank.com/contests/all-india-contest-by-mission-helix-a-25th-july/challenges/easy-3-cc */
+t = int(input())
+for _ in range(t):
+    s = input()
+    print(s.swapcase())
