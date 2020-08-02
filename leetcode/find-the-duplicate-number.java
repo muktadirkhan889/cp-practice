c831e38799bdf1ec2dde7b02e91fef6caf7e43a0
Muktadir Khan <muktadirkhan889@gmail.com>
2020-08-02 13:15:25 +0530

    Create find-the-duplicate-number.java

---
+++ leetcode/find-the-duplicate-number.java
@@ -0,0 +1,17 @@
+/* https://leetcode.com/problems/find-the-duplicate-number/ */
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int fast, slow;
+        fast = slow = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow!=fast);
+        slow = nums[0];
+        while(slow!=fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
