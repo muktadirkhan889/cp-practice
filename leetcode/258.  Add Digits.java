728c023e5ed81fefd88fc8fa5495435566287734
Muktadir Khan <muktadirkhan889@gmail.com>
2020-08-18 15:43:07 +0530

    Create 258.  Add Digits.java

---
+++ leetcode/258.  Add Digits.java
@@ -0,0 +1,11 @@
+// Space, Time: O(1)
+class Solution {
+    public int addDigits(int num) {
+        if(num == 0) {
+            return 0;
+        } else if(num % 9 == 0) {
+            return 9;
+        }
+        return num % 9;
+    }
+}
